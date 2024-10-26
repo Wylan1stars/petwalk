31c239fa7bf68316255b47f37c6b6cdfafd92e2d
Marcos <166460088+M4rk0Zoof@users.noreply.github.com>
2024-10-26 02:16:25 +0200

    Add files via upload

--- registro.php
+++ registro.php
@@ -5,9 +5,11 @@ $conn= mysqli_connect("localhost", "root", "", "petwalk");
 if(isset($_POST['regis'])){
     if(strlen($_POST['nombre']) >= 1 && strlen($_POST['email']) >= 1){
         $nombre = trim($_POST['nombre']);
+        $apellido = trim($_POST['apellido']);
         $email = trim($_POST['email']);
-        $fechareg = date("d/m/y");
-        $consul = "INSERT INTO login(nombre, email, fecha_reg) VALUES ('$nombre','$email','$fechareg')";
+        $contra = trim($_POST['contra']);
+        $direc = trim($_POST['direc']);
+        $consul = "INSERT INTO registrarse_propietario(nombre, apellido, correo, contraseña, direccion,) VALUES ('$nombre','$apellido','$email','$contra','$direc')";
         $resul = mysqli_query($conn, $consul);
         if($resul){
             echo "¡Te registraste!";
