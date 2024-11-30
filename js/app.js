f55ac6dc4cfbf23e3cee3e488ad2a8930a79494d
Dylan Fernandez <111945560+Wylan1stars@users.noreply.github.com>
2024-11-29 21:20:54 -0300

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// JavaScript para el menú hamburguesa
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('nav ul');
-
-    menuToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-    });
-});
